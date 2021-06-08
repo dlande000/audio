a9f064197655d081d57dafd8c939c3c734207893
David Anderson <dlande000@gmail.com>
2021-06-08 13:29:07 -0400

    Add template for audioController

---
+++ server/controllers/audioController.js
@@ -0,0 +1,13 @@
+const db = require('../models/audioModel');
+
+const audioController = {};
+
+audioController.getAudios = (req, res, next) => {
+  // do some work
+};
+
+audioController.postAudio = (req, res, next) => {
+  // do some work
+};
+
+module.exports = audioController;

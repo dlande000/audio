f5b5239ce491a614d5ee3298c3a050fafadf576d
David Anderson <dlande000@gmail.com>
2021-06-08 13:08:54 -0400

    Connect to DB

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 // const apiRouter = require('./routes/api');
 
 const app = express();
 const PORT = 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to Database');
+});
 
 app.get('/api/david', (req, res) => {
   res.send('this works');
